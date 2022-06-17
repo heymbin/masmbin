495becf2d17b96a42919767e61a6b4ef85d8c76d
heymbin <95487674+heymbin@users.noreply.github.com>
2022-06-18 03:31:42 +0700

    Add files via upload

---
+++ masmbin.js
@@ -0,0 +1,22 @@
+const h1 = document.querySelector("h1");
+function hoverIn1() {
+  document.body.style.backgroundColor = "#242F9B";
+  h1.style.color = "white";
+}
+function hoverIn2() {
+  document.body.style.backgroundColor = "#580559";
+  h1.style.color = "aliceblue";
+}
+function hoverIn3() {
+  document.body.style.backgroundColor = "#991818";
+  h1.style.color = "azure";
+}
+function hoverIn4() {
+  document.body.style.backgroundColor = "#1A379E";
+  h1.style.color = "cornsilk";
+}
+
+function hoverOut() {
+  document.body.style.backgroundColor = " #a344bd54";
+  h1.style.color = "#070910";
+}
